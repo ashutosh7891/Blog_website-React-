50bcfcfedcb3a63e2d01bd6ee111888150d9eda0
Ashutosh Yadav <ashutosh_yadav@Ashutoshs-MacBook-Air.local>
2024-03-20 13:19:14 +0530

    Login page

--- mega_blog/src/components/Login.jsx
+++ mega_blog/src/components/Login.jsx
@@ -21,28 +21,22 @@ function Login() {
 
     // login
 
-    const login = async(data) => [
-        setError('') // after login we need empty out error field so it does'nt interupt more
-
+    const login = async (data) => {
+        setError('')
         try {
-
             const session = await authService.login(data)
-
             if (session) {
                 const userData = await authService.getCurrentUser()
                 if (userData) {
-                    dispatch(authLogin(userdata))
-                    navigate('/') // home directory
-
+                    dispatch(authLogin(userData))
                 }
+                navigate('/')
             }
-            
         } catch (error) {
             setError(error.message)
-            
         }
-    ]
-
+    }
+ 
 
     
 
@@ -58,6 +52,53 @@ function Login() {
 
             </div>
             <h2 className='text-center text-2xl font-bold leading-tight '>Sign in to your account</h2>
+            <p className='mt-2 text-center text-base text-black/60'>
+
+                    Don&apos;t have an account?&nbsp;
+                    <Link 
+                    to= '/signup'
+                    className='font-medium text-primary transition-all duration-200 hover:underline'
+                    >
+                        Signup
+                    </Link>
+
+            </p>
+            {error && <p className='text-red-600 mt-8 text-center'> {error} </p>}
+            {/* form */}
+            <form 
+            onSubmit={handleSubmit(login)}
+            className='mt-8'
+            >
+                <div className='space-y-5'>
+
+                    <Input
+                    label = 'Email: '
+                    placeholder = 'Enter your email'
+                    type = 'Email'
+                    {...register('email' , {
+                        required : true,
+                        validate: {
+                            matchPattern: (value) => {
+                                /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                                "Email address must be a valid address"
+                            }
+                        }
+                    })}
+                    />
+                    <Input
+                    label = 'Password'
+                    placeholder = 'Enter your password'
+                    type = 'Password'
+                    {...register('password' , {
+                        required : true
+
+                    })}
+                    />
+                    <Buttons type='submit' className='w-full'>Sign In</Buttons>
+
+                </div>
+
+            </form>
         </div>
     </div>
   )
